b9df696633213aea198fa6879db73846fe373fc4
Pedro Miranda <pedro17rb@hotmail.com>
2026-08-01 07:09:24 -0300

    task7 finished

--- course3_data_collection_and_processing/Assignment2/task7.py
+++ course3_data_collection_and_processing/Assignment2/task7.py
@@ -1,14 +1,17 @@
 #Below, we have provided a species list and a population list. Use zip to combine these lists into one list of tuples called pop_info.
 # From this list, create a new list called endangered that contains the names of species whose populations are below 2500.
-
+import json
 
 species = ['golden retriever', 'white tailed deer', 'black rhino', 'brown squirrel', 'field mouse', 'orangutan', 'sumatran elephant', 'rainbow trout', 'black bear', 'blue whale', 'water moccasin', 'giant panda', 'green turtle', 'blue jay', 'japanese beetle']
 
 population = [10000, 90000, 1000, 2000000, 500000, 500, 1200, 8000, 12000, 2300, 7500, 100, 1800, 9500, 125000]
 
-pop_info = None   # Replace "None" with an expression that makes pop_info a list of tuples (each first item is a species string and each second item is a number, for that species' population)
-endangered = None # Replace "None" with an expression that makes `endangered` a list of strings (species whose population is below 2500)
+pop_info = list(zip(species, population))
+pop_info_json = json.dumps(list(zip(species, population)), indent=2)
+print(pop_info_json)
 
+endangered = [animal[0] for animal in pop_info if animal[1]<2500]
+print(endangered)
 
 
 assert ('golden retriever', 10000) in pop_info
